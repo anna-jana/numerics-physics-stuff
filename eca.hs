0f5ed0f1658b41506681b9267e7f9f62eb38f40e
Jana Riess <j.riess@wtnet.de>
2016-09-12 20:27:38 +0000

    elementary cellular automata

---
+++ eca.hs
@@ -0,0 +1,14 @@
+type Space = [Int]
+type Rule = [Int]
+
+step :: Rule -> Space -> Space
+step rl s = zipWith3 (\l c r -> rl !! (7 - 4*l - 2*c - r)) (last s:init s) s (tail s++[head s])
+
+rule :: Int -> Rule
+rule n = [mod (div n (2^i)) 2 | i <- [7,6..0]]
+
+disp :: Int -> Int -> IO ()
+disp n m = mapM_ (putStrLn . map (".#" !!)) $ take m $ iterate (step (rule n)) (let zeros = replicate m 0 in zeros ++ [1] ++ zeros)
+
+main :: IO ()
+main = disp 110 30
