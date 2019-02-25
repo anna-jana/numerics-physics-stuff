87ecd231a48be2baec767c5731f877ed8b05b484
Jana Riess <j.riess@wtnet.de>
2019-02-25 17:30:44 +0100

    comb

--- Combinatorics.hs
+++ Combinatorics.hs
@@ -9,3 +9,6 @@ permutations xs = [x:xs' | x <- xs, xs' <- permutations $ delete x xs]
 subsets :: Eq a => [a] -> [[a]]
 subsets xs = xs : concatMap subsets [delete x xs | x <- xs]
 subsets xs = xs : map (`delete` xs) xs >>= subsets
+
+perms :: Int -> [[Int]]
+perms n = foldl (\ps n' -> concatMap (\p -> map (\i -> take i p ++ [n'] ++ drop i p) [0..n']) ps) [[]] [0..n]
