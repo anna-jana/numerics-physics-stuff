dd94c0544c6455d23a5a82c7a30663d3eda820a5
Jana Riess <j.riess@wtnet.de>
2019-07-25 22:28:03 +0200

    metropolis

---
+++ metropolis.py
@@ -0,0 +1,18 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+
+def metropolis(E, T, r, x):
+    while True:
+        q = np.random.uniform(-1, 1, x.shape)
+        y = x + r * q
+        delta_E = E(y) - E(x)
+        p = min([1, np.exp(- delta_E / T)])
+        if np.random.rand() < p:
+            x = y
+        yield x
+
+if __name__ == "__main__":
+    steps = itertools.islice(metropolis(lambda x: x @ x / 2, 100.0, 10.0, np.array([1.0])), 1000)
+    plt.hist([s[0] for s in steps], 20, histtype="step")
+    plt.show()
