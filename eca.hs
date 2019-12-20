54ed3be2b8f86a97fdac786f9c07a5ce3622097a
Jana Riess <j.riess@wtnet.de>
2019-12-21 00:05:59 +0100

    cellular automata update

--- eca.hs
+++ eca.hs
@@ -2,7 +2,7 @@ type Space = [Int]
 type Rule = [Int]
 
 step :: Rule -> Space -> Space
-step rl s = zipWith3 (\l c r -> rl !! (7 - 4*l - 2*c - r)) (last s:init s) s (tail s++[head s])
+step rl s = zipWith3 (\l c r -> rl !! (7 - 4*l - 2*c - r)) (last s : init s) s (tail s ++ [head s])
 
 rule :: Int -> Rule
 rule n = [mod (div n (2^i)) 2 | i <- [7,6..0]]
--- game_of_life.py
+++ game_of_life.py
@@ -1,16 +1,22 @@
 import numpy as np
 
 def step(space):
-    neighbor_count = sum(np.roll(np.roll(space, dr, 0), dc, 1) for dr in (-1,0,1) for dc in (-1,0,1)) - space
-    return ((neighbor_count == 3) | ((space == 1) & (neighbor_count == 2))).astype(np.int)
+    neighbor_count = sum(np.roll(space, (dr, dc), (0, 1)) for dr in (-1,0,1) for dc in (-1,0,1)) - space
+    return ((neighbor_count == 3) | (space & (neighbor_count == 2))).astype(np.int)
 
-# space = np.random.choice([0,1], (10,10))
+def show(space):
+    for i in range(space.shape[0]):
+        for j in range(space.shape[1]):
+            print("#" if space[i, j] else ".", end="")
+        print("")
+    print("")
 
-space = np.zeros((5, 5), dtype=np.int)
-space[:3, :3] = np.array([[0, 0, 1],
-                          [1, 0, 1],
-                          [0, 1, 1]])
+if __name__ == "__main__":
+    space = np.zeros((5, 5), dtype=np.int)
+    space[:3, :3] = np.array([[0, 0, 1],
+                              [1, 0, 1],
+                              [0, 1, 1]])
 
-for i in range(10):
-    print(space, "\n")
-    space = step(space)
+    for i in range(10):
+        show(space)
+        space = step(space)
