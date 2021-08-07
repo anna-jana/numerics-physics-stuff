8a27e785fba9f0447568084fc16acd3d7b2131c1
Jana Riess <j.riess@wtnet.de>
2021-08-07 18:15:50 +0200

    fixed bug in PIQMC

--- quantum_mc.py
+++ quantum_mc.py
@@ -7,9 +7,7 @@ import vegas
 def calc_euclidean_action(config, x_i, x_f, m, a, V_fn):
     kin = m/(2*a**2) * (np.sum((config[1:] - config[:-1])**2)
                    + (x_i - config[0])**2 + (config[-1] - x_f)**2) # boundary
-    #pot = 0.5 * (np.sum(V_fn(config[1:]) + V_fn(config[:-1]))
-    #           + (V_fn(config[0]) + V_fn(x_i)) + (V_fn(x_f) + V_fn(config[-1]))) # boundary
-    pot = 0.5 * np.sum(V_fn(config)) + 0.5 * V_fn(x_i) + 0.5 * V_fn(x_f)
+    pot = np.sum(V_fn(config)) + V_fn(x_i) + V_fn(x_f)
     return a * (kin + pot)
 
 def make_path_integrant(m, a, N, x_i, x_f, V_fn):
@@ -55,7 +53,7 @@ ans = np.array([calc_prop(x, V_harm) for x in tqdm(x_vals)])
 prop_vals, prop_err = ans.T
 analytic = calc_prop_analytic(x_range)
 plt.plot(x_range, analytic, label="analytic")
-norm = 1 / prop_vals[0] * analytic[0] # cheat by normalizing by the first analytical result
+norm = 1 # 1 / prop_vals[0] * analytic[0] # cheat by normalizing by the first analytical result
 plt.errorbar(x_vals, prop_vals * norm, fmt="o", yerr=prop_err, label="numerical path integral")
 plt.xlim(0,2)
 plt.ylim(0,0.1)
