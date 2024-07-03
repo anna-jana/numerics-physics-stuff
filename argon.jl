b233c061ef096346037c46715f134312eaa24132
Anna-Jana Riess <j.riess@wtnet.de>
2024-07-03 22:59:56 +0200

    fixed argon

--- argon.jl
+++ argon.jl
@@ -6,35 +6,22 @@ using Random
 
 # unit of mass such that the mass of a particle is 1
 # the unit of length is sigma
-# hence the size of each box is 1
 # the unit of time is (m * sigma^2 / epsilon)**0.5
+# the unit of temperature is epsilon * kB
 
 mutable struct Particle
     x :: MVector{3, Float64}
     v :: MVector{3, Float64}
     F :: MVector{3, Float64}
     F_old :: MVector{3, Float64}
-    box_index :: CartesianIndex{3}
-end
-
-struct Box
-    particle_indicies :: Vector{Int}
-    box_index :: CartesianIndex{3}
 end
 
 mutable struct Simulation
     particles :: Vector{Particle}
-    boxes :: Array{Box, 3}
     const L :: Float64
     const dt :: Float64
     const T :: Float64
     step :: Int
-end
-
-function add_particle_to_box!(index::Int, particle::Particle, simulation::Simulation)
-    i = CartesianIndex(floor.(Int, particle.x) .+ 1...)
-    push!(simulation.boxes[i].particle_indicies, index)
-    particle.box_index = i
 end
 
 function foreach_particle_pair(f::Function, simulation::Simulation)
@@ -52,38 +39,6 @@ function foreach_particle_pair(f::Function, simulation::Simulation)
             end
             d = norm(diff)
             f(p, q, diff, d)
-        end
-    end
-
-    return
-
-    already_conputed = falses(length(simulation.particles), length(simulation.particles))
-
-    for (particle_index, p) in enumerate(simulation.particles)
-        @inbounds for i in p.box_index[1]-1 : p.box_index[1]+1
-            for j in p.box_index[2]-1 : p.box_index[2]+1
-                for k in p.box_index[3]-1 : p.box_index[3]+1
-                    i = mod1(i, size(simulation.boxes, 1))
-                    j = mod1(j, size(simulation.boxes, 2))
-                    k = mod1(k, size(simulation.boxes, 3))
-
-                    for other_particle_index in 1:length(simulation.boxes[i, j, k].particle_indicies)
-                        if !already_conputed[particle_index, other_particle_index] && particle_index != other_particle_index
-                            already_conputed[other_particle_index, particle_index] = true
-                            q = simulation.particles[other_particle_index]
-                            diff = p.x - q.x
-                            # minimum image convetion
-                            for i in 1:3
-                                if abs(diff[i]) > simulation.L / 2
-                                    diff[i] -= round(Int, diff[i] / simulation.L) * simulation.L
-                                end
-                            end
-                            d = norm(diff)
-                            f(p, q, diff, d)
-                        end
-                    end
-                end
-            end
         end
     end
 end
@@ -122,14 +77,6 @@ function make_step!(simulation::Simulation)
         p.v += simulation.dt * (p.F + p.F_old) * 0.5
     end
 
-    for box in simulation.boxes
-        empty!(box.particle_indicies)
-    end
-
-    for (i, particle) in enumerate(simulation.particles)
-        add_particle_to_box!(i, particle, simulation)
-    end
-
     simulation.step += 1
 
     return V
@@ -157,13 +104,11 @@ function run(rho, T, M)
     rescale_every_n_steps = 10
     production_time = 20.0
 
-    nboxes = ceil(Int, L)
-    boxes = [Box(Int[], i) for i in CartesianIndices((nboxes, nboxes, nboxes))]
-    simulation = Simulation([], boxes, L, dt, T, 0)
+    simulation = Simulation([], L, dt, T, 0)
 
     function add_particle_at(x)
         v = MVector(randn(), randn(), randn()) * sqrt(T) # maxwell distribution
-        push!(simulation.particles, Particle(x, v, zero(MVector{3, Float64}), zero(MVector{3, Float64}), CartesianIndex(-1, -1, -1)))
+        push!(simulation.particles, Particle(x, v, zero(MVector{3, Float64}), zero(MVector{3, Float64})))
     end
 
     dx = L / M # periodic bc
@@ -179,10 +124,6 @@ function run(rho, T, M)
     total_velocity_per_particle = total_veloctiy / length(simulation.particles)
     for p in simulation.particles
         p.v -= total_velocity_per_particle
-    end
-
-    for (i ,particle) in enumerate(simulation.particles)
-        add_particle_to_box!(i, particle, simulation)
     end
 
     compute_forces!(simulation)
@@ -220,7 +161,7 @@ using Statistics
 M = 3
 rho = 0.8
 T0 = 1.0
-E_kins, E_pots, virials = Argon.run(rho, T0, M)
+@time E_kins, E_pots, virials = Argon.run(rho, T0, M)
 N = 4*M^3
 
 T = mean(E_kins) / ((N - 1)*3/2)
