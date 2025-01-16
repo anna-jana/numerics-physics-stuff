cecd53b2fc9d8f8e72a6eed5669ce5acd90f7f14
Anna-Jana Riess <j.riess@wtnet.de>
2025-01-16 13:00:47 +0100

    fixed typo

--- semi_empirical_mass_formula.py
+++ semi_empirical_mass_formula.py
@@ -17,8 +17,8 @@ E_vol = a_V * A
 E_surf = a_surf * A**(2/3)
 E_coloumb = a_c * Z * (Z - 1) * A**(-1/3)
 E_sym = a_s * (N - Z)**2 / (4*A)
-E_pair = (np.where((Z % 2 == 0) % (N % 2 == 0), +1,
-   np.where((Z % 2 == 1) % (N % 2 == 1), -1, 0))) * a_p * A**(-1/2)
+E_pair = (np.where((Z % 2 == 0) & (N % 2 == 0), +1,
+   np.where((Z % 2 == 1) & (N % 2 == 1), -1, 0))) * a_p * A**(-1/2)
 E = E_vol - E_surf - E_coloumb - E_sym + E_pair
 
 plt.figure()
